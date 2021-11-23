66c4deb7a73379519fa3eb588f305dbd2551b4d8
dezaelDev <emmanuelfernandez.developer@gmail.com>
2021-11-22 21:57:27 -0300

    se agrega relaciones de tabla post a tabla usuario

--- scripts/blog.sql
+++ scripts/blog.sql
@@ -1,21 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
+-- version 4.3.11
+-- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-11-2021 a las 01:56:03
--- Versión del servidor: 10.4.19-MariaDB
--- Versión de PHP: 8.0.6
+-- Tiempo de generación: 23-11-2021 a las 01:56:09
+-- Versión del servidor: 5.6.24
+-- Versión de PHP: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `blog`
@@ -27,7 +26,7 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `post`
 --
 
-CREATE TABLE `post` (
+CREATE TABLE IF NOT EXISTS `post` (
   `id_post` int(11) NOT NULL,
   `autor_post` varchar(50) NOT NULL,
   `fecha_post` date NOT NULL,
@@ -35,16 +34,17 @@ CREATE TABLE `post` (
   `titulo_post` varchar(150) NOT NULL,
   `imagen_post` text NOT NULL,
   `descripcion_post` text NOT NULL,
-  `contenido_completo_post` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `contenido_completo_post` text NOT NULL,
+  `id_userFK` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `post`
 --
 
-INSERT INTO `post` (`id_post`, `autor_post`, `fecha_post`, `categoria_post`, `titulo_post`, `imagen_post`, `descripcion_post`, `contenido_completo_post`) VALUES
-(3, 'Dario Olmedo', '2021-11-18', 'Programacion', 'Probando', 'diagrama.png', 'probando post', '<p>probando post</p>'),
-(4, 'Dario Olmedo', '2021-11-18', 'Otra', 'Probando por segunda ves', 'mapa.png', 'esta es la segunda', '<div>\r\n<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dicta aperiam quos! Accusantium, quasi corrupti error, ut ullam nihil molestiae eaque architecto, nemo illo explicabo! Sit id enim hic voluptatum.</div>\r\n</div>');
+INSERT INTO `post` (`id_post`, `autor_post`, `fecha_post`, `categoria_post`, `titulo_post`, `imagen_post`, `descripcion_post`, `contenido_completo_post`, `id_userFK`) VALUES
+(3, 'Dario Olmedo', '2021-11-18', 'Programacion', 'Probando', 'diagrama.png', 'probando post', '<p>probando post</p>', 1),
+(4, 'Dario Olmedo', '2021-11-18', 'Otra', 'Probando por segunda ves', 'mapa.png', 'esta es la segunda', '<div>\r\n<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dicta aperiam quos! Accusantium, quasi corrupti error, ut ullam nihil molestiae eaque architecto, nemo illo explicabo! Sit id enim hic voluptatum.</div>\r\n</div>', 8);
 
 -- --------------------------------------------------------
 
@@ -52,10 +52,10 @@ INSERT INTO `post` (`id_post`, `autor_post`, `fecha_post`, `categoria_post`, `ti
 -- Estructura de tabla para la tabla `rol`
 --
 
-CREATE TABLE `rol` (
+CREATE TABLE IF NOT EXISTS `rol` (
   `id_rol` int(11) NOT NULL,
   `rol` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `rol`
@@ -71,14 +71,14 @@ INSERT INTO `rol` (`id_rol`, `rol`) VALUES
 -- Estructura de tabla para la tabla `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(9) NOT NULL,
   `name` varchar(25) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `register_date` varchar(8) NOT NULL,
   `id_rolFK` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `users`
@@ -108,8 +108,7 @@ ALTER TABLE `rol`
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_rolFK` (`id_rolFK`);
+  ADD PRIMARY KEY (`id`), ADD KEY `id_rolFK` (`id_rolFK`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -119,20 +118,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `post`
 --
 ALTER TABLE `post`
-  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- Restricciones para tablas volcadas
 --
@@ -141,8 +137,7 @@ ALTER TABLE `users`
 -- Filtros para la tabla `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_rolFK`) REFERENCES `rol` (`id_rol`);
-COMMIT;
+ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_rolFK`) REFERENCES `rol` (`id_rol`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
