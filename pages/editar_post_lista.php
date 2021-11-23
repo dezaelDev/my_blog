f3f5b4b5354f3e156fa7d44ab02966fdbdcfb747
Olm-Dario <dariolmedo2@gmail.com>
2021-11-22 21:28:47 -0300

    Add files via upload

---
+++ pages/editar_post_lista.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/editar_post.css">
+    <title>Post</title>
+</head>
+<body>
+    <div class="container-table">
+        
+        <div class="table__title">Posteos</div>
+
+        <div class="table__header">Titulo</div>
+
+        <div class="table__header">Editar</div>
+
+        <div class="table__header">Eliminar</div>
+
+        <div class="table__item">Prueba</div>
+
+        <div class="table__item">si</div>
+
+        <div class="table__item">si</div>
+    </div>
+</body>
+</html>
